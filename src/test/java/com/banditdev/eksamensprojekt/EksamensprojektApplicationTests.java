f6e8fd82c13cea1951e66b24dce231366095835e
davidek-d <dajo1002@stud.ek.dk>
2026-04-29 12:38:32 +0200

    initial commit. lavet repository

---
+++ src/test/java/com/banditdev/eksamensprojekt/EksamensprojektApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banditdev.eksamensprojekt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EksamensprojektApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
