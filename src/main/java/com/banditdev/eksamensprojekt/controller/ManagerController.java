34d013ce7eae4b6e0c4d25afea732d4ebddbd52c
TOKJ <tokj0001@stud.ek.dk>
2026-05-06 13:24:26 +0200

    Test commit - Logintamtam

--- src/main/java/com/banditdev/eksamensprojekt/controller/ManagerController.java
+++ src/main/java/com/banditdev/eksamensprojekt/controller/ManagerController.java
@@ -1,4 +1,44 @@
 package com.banditdev.eksamensprojekt.controller;
 
+import com.banditdev.eksamensprojekt.service.ManagerService;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+@RequestMapping("profile")
 public class ManagerController {
+    private final ManagerService managerService;
+
+    public ManagerController(ManagerService managerService) {
+        this.managerService = managerService;
+    }
+
+    private boolean isLoggedIn(HttpSession session) {
+        return session.getAttribute("user") != null;
+    }
+
+    @GetMapping("/login")
+    public String login(HttpSession session) {
+        if (session.getAttribute("manager") == null) {
+            return "loginPage";
+        }
+        return "redirect:/profileOverview";
+    }
+
+    @PostMapping("/authenticateLogin")
+    public String login(@RequestParam String managerUsername, @RequestParam String managerPassword, HttpSession session) {
+
+        if (managerService.validateUser(managerUsername, managerPassword)) {
+            session.setAttribute("user", managerService.findManagerByUsername(managerUsername));
+            return "redirect:/profile/loginPage";
+        } else {
+            return "redirect:/profile/profileOverview";
+        }
+    }
 }
--- src/main/java/com/banditdev/eksamensprojekt/service/ManagerService.java
+++ src/main/java/com/banditdev/eksamensprojekt/service/ManagerService.java
@@ -1,4 +1,14 @@
 package com.banditdev.eksamensprojekt.service;
 
+
+import org.springframework.stereotype.Service;
+
+@Service
 public class ManagerService {
+
+
+    //TODO validateUser()
+    //TODO findManagerByUsername()
+
+
 }
