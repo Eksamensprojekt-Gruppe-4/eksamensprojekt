19534fd48cad868f4dc1a019edeb039556add8dd
davidek-d <dajo1002@stud.ek.dk>
2026-05-18 12:50:09 +0200

    controller tests setup

---
+++ src/test/java/com/banditdev/eksamensprojekt/controller/RootControllerTest.java
@@ -0,0 +1,12 @@
+package com.banditdev.eksamensprojekt.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RootControllerTest {
+
+    @Test
+    void home() {
+    }
+}
