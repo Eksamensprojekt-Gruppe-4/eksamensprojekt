726d8d4f9639b5015bf38188351afaceddbab1e0
TOKJ <tokj0001@stud.ek.dk>
2026-05-18 12:08:52 +0200

    Logic for userController moved to service

--- src/main/java/com/banditdev/eksamensprojekt/controller/SubProjectController.java
+++ src/main/java/com/banditdev/eksamensprojekt/controller/SubProjectController.java
@@ -89,6 +89,7 @@ public class SubProjectController {
         model.addAttribute("tasksBySubProject", tasks);
         model.addAttribute("usersById", userService.getUsersMappedById());
 
+
         return "subProjectView";
     }
 }
